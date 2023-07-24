83bfcf244203fda21a02a20bf45b2d64b29045b4
Michael Vilabrera <m.vilabrera@gmail.com>
2023-07-24 12:06:29 -0500

    Initial Commit

---
+++ IBBasics/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  IBBasics
+//
+//  Created by admin on 7/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var mainButton: UIButton!
+    
+    // MARK: - Instance Properties
+    
+    // MARK: - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // MARK: - IBActions
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+    }
+    
+    // MARK: - Instance Methods
+    
+}
+

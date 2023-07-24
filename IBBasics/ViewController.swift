f99752313d739f0598f4766032f48467a669eb99
Michael Vilabrera <m.vilabrera@gmail.com>
2023-07-24 13:59:24 -0500

    messages are required to commit

--- IBBasics/ViewController.swift
+++ IBBasics/ViewController.swift
@@ -14,16 +14,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainButton: UIButton!
     
     // MARK: - Instance Properties
+    // TODO: - create boolean property & set value
+    var appState = true
     
     // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mainButton.setTitle("Tap Me!", for: .normal)
     }
 
     // MARK: - IBActions
     @IBAction func buttonTapped(_ sender: Any) {
+        // TODO: - create conditional to toggle between boolean states
+        if appState {
+            textLabel.text = "I am learning to build apps"
+            self.view.backgroundColor = .white
+        } else {
+            textLabel.text = "This app is AWESOME!"
+            self.view.backgroundColor = .green
+        }
+        appState.toggle()
         
+//        textLabel.text = "This app is AWESOME!"
+//        let background = self.view.backgroundColor
+//        
+//        if background == UIColor.green {
+//            self.view.backgroundColor = UIColor.lightGray
+//        } else {
+//            self.view.backgroundColor = UIColor.green
+//        }
+//        self.view.backgroundColor = UIColor.green
     }
     
     // MARK: - Instance Methods
